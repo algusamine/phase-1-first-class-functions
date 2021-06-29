be1ddec97b585662273665800b7dac012f9e00f4
Amine <algusamine@gmail.com>
2021-06-29 01:01:08 -0400

    commit tree 1f8a02ab9ef25b455d0f7f98ec8691b166906455

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function receivesAFunction(spy){
+    return spy()
+  }
+  
+  /*function returnsAnAnonymousFunction(){
+    return function(){
+    }
+  }*/
+  function returnsANamedFunction(){
+      return function expect(){
+
+      }
+  }
+
+  function returnsAnAnonymousFunction(){
+      return function(){
+          
+      }
+  }
